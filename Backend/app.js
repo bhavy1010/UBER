5a777160de0cc84752d97a071c6192f40ea17f0e
Bhavy Patel <bhavypatel101005@gmail.com>
2026-04-27 09:47:10 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+const connectToDb = require("./db/db");
+
+connectToDb();
 
 app.use(cors());
 
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer(app);
 
+
 server.listen(port , () =>{
     console.log(`server is running on port ${port}`);
 });
